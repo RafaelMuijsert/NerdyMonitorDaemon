ac0c5eb2c756d597ca052eba5a9df96f525497ed
Rafael Muijsert <rmuijsert@gmail.com>
2023-05-09 12:43:08 +0200

    added disk measurement

--- bin/disk-space.sh
+++ bin/disk-space.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "50GB/200GB used"
+used=$(df --total | grep ^total | awk '{ print $3 }')
+available=$(df --total | grep ^total | awk '{ print $4 }')
+use_percent=$(df --total | grep ^total | awk '{ print $5 }')
 
+echo "$used used (${use_percent}), ${available} available "
